13ceb6a1d0a685eb375da10f23823c8218e1272b
totuflatkokur <totuflatkokur@totuflatkokur.is>
2026-07-09 21:18:51 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,3 +4,6 @@ const SUPABASE_ANON_KEY = "PUBLISHABLE_LYKILLINN_ÞINN";
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const ADMIN_PASSWORD = "1234";
+db.from("employees").select("*").then(({ data, error }) => {
+  alert(error ? "Supabase villa: " + error.message : "Starfsmenn fundust: " + data.length);
+});
