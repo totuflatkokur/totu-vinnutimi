7ef70de1184b13ae4e916e536947d787cc4dd218
totuflatkokur <totuflatkokur@totuflatkokur.is>
2026-07-09 21:36:08 +0000

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-// TótuOS Enterprise v5 Supabase tenging
+// Tótu Stimpilklukka v1
 // Settu publishable key-inn þinn í SUPABASE_ANON_KEY.
 
 const SUPABASE_URL = "https://rzvcprkdriiywupaqupv.supabase.co";
@@ -6,5 +6,5 @@ const SUPABASE_ANON_KEY = "SETTU_PUBLISHABLE_KEY_HER";
 
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Breyttu þessu lykilorði ef þú vilt.
+// Breyttu þessu lykilorði.
 const ADMIN_PASSWORD = "1234";
