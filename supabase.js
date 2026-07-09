596cc9386c3b01c911ad3d1dc46464b5f5c83dbe
totuflatkokur <totuflatkokur@totuflatkokur.is>
2026-07-09 21:16:16 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,6 @@
-// Settu þín Supabase gildi hér ef þau eru ekki nú þegar í skránni þinni.
-// Ef gamla supabase.js virkar nú þegar, þá má halda henni og ekki nota þessa.
-
-const SUPABASE_URL = window.SUPABASE_URL || "SETTU_SUPABASE_URL_HER";
-const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || "SETTU_SUPABASE_ANON_KEY_HER";
+const SUPABASE_URL = "https://rzvcprkdriiywupaqupv.supabase.co";
+const SUPABASE_ANON_KEY = "PUBLISHABLE_LYKILLINN_ÞINN";
 
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Stjórnanda lykilorð. Breyttu þessu.
-const ADMIN_PASSWORD = window.ADMIN_PASSWORD || "1234";
+const ADMIN_PASSWORD = "1234";
